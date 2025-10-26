34d8593f8349b681663d18f4a4117938b75bd5b5
NINA08042 <18851717287@163.com>
2025-10-26 21:14:47 +0000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div className="page">
       <h1>Welcome to Group A PostgreSQL Learning Hub</h1>
-      <h2>Jianing Xue    B01812701		Xinrui Gao B01819974
-Weixue Kong  B01819838		Fang Tang B01814427</h2>
+      <h2>Jianing Xue    B01812701	|	Xinrui Gao B01819974|
+Weixue Kong  B01819838	|	Fang Tang B01814427</h2>
 
       
       <p>This website includes a PostgreSQL presentation, setup tutorial, and code explanations.</p>
