b7dcc8fb779c7a43612679506aa248d0f91053ce
NINA08042 <18851717287@163.com>
2025-10-26 15:30:19 +0000

    Update Presentation.tsx

--- src/pages/Presentation.tsx
+++ src/pages/Presentation.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 
+// 让 Markdown 能识别 <video> 标签
+import rehypeRaw from "rehype-raw";
+
 export default function Presentation() {
   const [content, setContent] = useState("");
 
   useEffect(() => {
-    fetch(`${import.meta.env.BASE_URL}src/assets/postgresql-presentation.md`)
-      .then(res => res.text())
+    // 推荐使用 public 文件夹路径，最稳妥
+    fetch("postgresql-presentation.md")
+      .then((res) => res.text())
       .then(setContent);
   }, []);
 
   return (
     <div className="page markdown-body">
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown rehypePlugins={[rehypeRaw]}>{content}</ReactMarkdown>
     </div>
   );
 }
