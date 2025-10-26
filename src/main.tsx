3f83008052bc7b7c36cfdda349cb7a4620ce3b4d
NINA08042 <18851717287@163.com>
2025-10-26 15:27:34 +0000

    Simplify routing by removing Routes and individual routes
    Removed Routes and individual Route components, simplifying the routing structure.

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Presentation from "./pages/Presentation";
-import Tutorial from "./pages/Tutorial";
-import CodeExplanation from "./pages/CodeExplanation";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Group-A-PostgreSQL">
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/presentation" element={<Presentation />} />
-        <Route path="/tutorial" element={<Tutorial />} />
-        <Route path="/code" element={<CodeExplanation />} />
-      </Routes>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
