8292b8a7b50a8f1b6f4bfbfbea8887d14cbcaadf
NINA08042 <18851717287@163.com>
2025-10-26 12:54:00 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Presentation from "./pages/Presentation";
 import Tutorial from "./pages/Tutorial";
 import CodeExplanation from "./pages/CodeExplanation";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <Navbar />
+    <div>
+      <nav className="navbar">
+        <h2>Group A: PostgreSQL</h2>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/presentation">Presentation</Link></li>
+          <li><Link to="/tutorial">Tutorial</Link></li>
+          <li><Link to="/code">Code Explanation</Link></li>
+        </ul>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/presentation" element={<Presentation />} />
         <Route path="/tutorial" element={<Tutorial />} />
         <Route path="/code" element={<CodeExplanation />} />
       </Routes>
-      <Footer />
-    </>
+
+      <footer>© 2025 Group A | PostgreSQL Learning Hub</footer>
+    </div>
   );
 }
-
-export default App;
