fbf0ab13a2d661f5746ecd6d30d883ce0a49796c
NINA08042 <18851717287@163.com>
2025-10-26 15:00:37 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import Presentation from "./pages/Presentation";
+import Tutorial from "./pages/Tutorial";
+import CodeExplanation from "./pages/CodeExplanation";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
+    <BrowserRouter basename="/Group-A-PostgreSQL">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/presentation" element={<Presentation />} />
+        <Route path="/tutorial" element={<Tutorial />} />
+        <Route path="/code" element={<CodeExplanation />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
