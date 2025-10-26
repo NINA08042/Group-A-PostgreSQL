b3747d4804d76e39edc16b3f2ff364117e048600
NINA08042 <18851717287@163.com>
2025-10-26 15:40:14 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// 检测当前是否是部署环境
+const isProd = process.env.NODE_ENV === 'production'
+
+// 在本地环境中 base 是 '/'，在 GitHub Pages 部署时是 '/Group-A-PostgreSQL/'
 export default defineConfig({
-  base: "/Group-A-PostgreSQL/",
   plugins: [react()],
-  assetsInclude: ["**/*.md"]
-});
+  base: isProd ? '/Group-A-PostgreSQL/' : '/',
+  server: {
+    open: true, // 启动自动打开浏览器
+  },
+})
