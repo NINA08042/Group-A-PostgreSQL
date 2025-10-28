3966dd3991d15232c0a2193af3d0d450937c2bec
NINA08042 <18851717287@163.com>
2025-10-28 11:46:59 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
+import Welcome from "./pages/Welcome";
 import Home from "./pages/Home";
 import Presentation from "./pages/Presentation";
 import Tutorial from "./pages/Tutorial";
 import CodeExplanation from "./pages/CodeExplanation";
-import Welcome from "./pages/Welcome";
 
-function App() {
+export default function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Welcome />} /> 
+        <Route path="/" element={<Welcome />} />
         <Route path="/home" element={<Home />} />
         <Route path="/presentation" element={<Presentation />} />
         <Route path="/tutorial" element={<Tutorial />} />
         <Route path="/code-explanation" element={<CodeExplanation />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Welcome />} />
       </Routes>
       <Footer />
     </>
   );
 }
-
-export default App;
