3de92eb8d1baa3cba4c9ed56bec467421364a689
NINA08042 <18851717287@163.com>
2025-10-28 10:45:29 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Presentation from "./pages/Presentation";
 import Tutorial from "./pages/Tutorial";
@@ -11,13 +13,17 @@ function App() {
   return (
     <>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/presentation" element={<Presentation />} />
         <Route path="/tutorial" element={<Tutorial />} />
         <Route path="/code-explanation" element={<CodeExplanation />} />
-        <Route path="*" element={<Home />} />
+
+        {/* ✅ Default redirect to Home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+
       <Footer />
     </>
   );
