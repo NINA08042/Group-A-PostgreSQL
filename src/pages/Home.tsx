eadbad3b55c059e93625b9a7db56f6d8fa5f8cb3
NINA08042 <18851717287@163.com>
2025-10-28 11:31:12 +0000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Group A：CW01 — PostgreSQL</title>
+  </head>
+  <body>
+ <h1>Welcome to Group A: CW01 -- PostgreSQL</h1>
+    <h2>Click the tab to start!</h2>
+  </body>
+</html>
+
+
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
