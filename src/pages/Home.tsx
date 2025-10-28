df0da12b1ad8c2a4ff72acb5792eb92b4236780e
NINA08042 <18851717287@163.com>
2025-10-28 09:26:26 +0000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,23 @@
-export default function Home() {
-  return (
-    <div className="page">
-      <h1>Welcome to Group A PostgreSQL Learning Hub</h1>
-      <h2>Jianing Xue    B01812701	|	Xinrui Gao B01819974|
-Weixue Kong  B01819838	|	Fang Tang B01814427</h2>
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
+import homeMd from "../assets/home.md?raw";
 
-      
-      <p>This website includes a PostgreSQL presentation, setup tutorial, and code explanations.</p>
-      <p>Use the navigation bar above to explore.</p>
+export default function Home() {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    setContent(homeMd);
+  }, []);
+
+  return (
+    <div className="page markdown-body">
+      <ReactMarkdown
+        children={content}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+      />
     </div>
   );
 }
