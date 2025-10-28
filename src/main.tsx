1a810ba2d8358fac1b76ee9461c2fc55cad6725c
NINA08042 <18851717287@163.com>
2025-10-28 10:53:05 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Group-A-PostgreSQL/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
