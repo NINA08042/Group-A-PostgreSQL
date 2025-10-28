66a14d2cf29d14ee4545f12246b8ce55559b2394
NINA08042 <18851717287@163.com>
2025-10-28 11:43:42 +0000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,11 @@ export default function Navbar() {
     <nav className="navbar">
       <h2>Group A - CW01: PostgreSQL</h2>
       <ul>
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/">Welcome</Link></li>
+        <li><Link to="/home">Home</Link></li>
         <li><Link to="/presentation">Presentation</Link></li>
         <li><Link to="/tutorial">Tutorial</Link></li>
-        <li><Link to="/code">Code Explanation</Link></li>
+        <li><Link to="/code-explanation">Code Explanation</Link></li>
       </ul>
     </nav>
   );
